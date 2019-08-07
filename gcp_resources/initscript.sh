d737c3da5683c3d9b2cbd7209786d9303cb4e99b
Saravanan G <g.gsaravanan@gmail.com>
2019-08-07 13:18:43 +0530

    Update initscript.sh

--- gcp_resources/initscript.sh
+++ gcp_resources/initscript.sh
@@ -3,6 +3,7 @@
 # This scripts installs wget, ansible and git packages
 mkdir -p /data/installers
 mkdir -p /data/ansible
+mkdir -p /data/playbooks
 cd /data/installers/
 rm -rf /data/ansible/*
 outfile='/var/log/userdata.out'
@@ -22,3 +23,19 @@ if [ ! -f /bin/git ] ; then
 yum install git -y >> $outfile
 fi
 
+# Clone rpm creation repo
+cd /data/playbooks
+echo "Creating dummmy rpm" >> $outfile
+git clone https://github.com/chefgs/create_dummy_rpm.git >> $outfile
+cd create_dummy_rpm
+chmod +x create_rpm.sh
+./create_rpm.sh spec_file/my-monitoring-agent.spec >> $outfile
+
+# Clone Ansible repo
+cd /data/ansible
+echo "Playbook Repo cloning" >> $outfile 
+git clone https://github.com/chefgs/ansible_playbooks.git >> $outfile
+
+echo "Executing Playbook" >> $outfile
+cd /data/ansible/ansible_playbooks/cloud_init_playbook/
+ansible-playbook playbook.yml -i ansible/hosts.ini >> /var/log/ansiblerun.out
