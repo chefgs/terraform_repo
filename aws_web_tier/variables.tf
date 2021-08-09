127d4e30622b117c95f29e33e0e0d51d0f889af4
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2021-08-09 11:47:41 +0530

    variables updated

--- aws_web_tier/variables.tf
+++ aws_web_tier/variables.tf
@@ -1,8 +1,18 @@
 variable "region" {
 default = "us-west-2"
 }
-variable "instance_type" {}
-variable "creds" {}
-variable "instance_key" {}
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "creds" {
+default = "~/.aws/credentials"
+}
+variable "instance_key" {
+default = "aws_ec2_pem_file_name2"
+}
+variable "vpc_cidr" {
+default = "178.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+default = "178.0.10.0/24"
+}
