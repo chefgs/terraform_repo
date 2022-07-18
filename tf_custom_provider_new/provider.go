0eb85ae284784a41bb7a351e505e0478f2f32313
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2022-07-18 15:35:39 +0530

    recent updates

--- tf_custom_provider_new/provider.go
+++ tf_custom_provider_new/provider.go
@@ -2,13 +2,13 @@
 package main
 
 import (
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func Provider() *schema.Provider {
-        return &schema.Provider{
-                ResourcesMap: map[string]*schema.Resource{
-                     "example_server": resourceServer(),
-                },
-        }
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"example_server": resourceServer(),
+		},
+	}
 }
