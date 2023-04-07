37b8be83118032f04bc5bf48d4b232068673d653
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2023-04-08 01:09:23 +0530

    updated ami for demo

--- youtube_tf_demo/ec2.tf
+++ youtube_tf_demo/ec2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0fcf52bcf5db7b003"
   instance_type = "t2.micro"
 
   tags = {
