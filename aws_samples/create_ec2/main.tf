8632f8ae02a18e49809d07796f04e2960aa21eff
Saravanan G <g.gsaravanan@gmail.com>
2023-06-09 18:17:40 +0530

    Added comments in code create_ec2

--- aws_samples/create_ec2/main.tf
+++ aws_samples/create_ec2/main.tf
@@ -1,3 +1,6 @@
+# Variables Block
+# Common values used across the terraform code can be added as variables
+# We can override the values using .tfvars files while running terraform plan/apply
 variable "region" {
 default = "us-west-2"
 }
@@ -10,6 +13,10 @@ variable "instance_count" {
   default = 2
 }
 
+# Terraform Required provider Block
+# In this section, we need to declare the providers and their version constraint used to create the infrastructure
+# It is needed to avoid any version mismatch of the provider 
+# Also it is good to mention what is the required version of Terraform CLI needed for the infra creation
 terraform {
   required_providers {
     aws = {
@@ -21,16 +28,24 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+# Provider block declares the provider on which the infra will be created
+# For AWS, one way of doing the cred authentication is to install AWS CLI and configure it to add access_key_id and secret_access_key
 provider "aws" {
   profile = "default"
   region  = var.region
 }
 
+# Resource Block
+# In this section, we will add the resources that we will be adding and managing in Cloud infra
+# 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  
+  # In terraform conditional statements are possible
   count = var.instance_count_needed ? var.instance_count : 1
 
+  # We can use the provisioners like user_data to run scripts that will be executed when the instance is getting created.
   user_data = <<-EOF
   #!/bin/bash
   echo "This script was executed from user_data"
@@ -41,6 +56,9 @@ resource "aws_instance" "app_server" {
   }
 }
 
+# Output Block
+# Here we can print the values of Infra resources that is supported
+# For ex: We are printing instance_id and instance_state
 output "instance_id" {
   description = "ID of the EC2 instance(s)"
   value       = aws_instance.app_server.*.id
